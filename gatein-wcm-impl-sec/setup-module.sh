df963a8ae66a15b25a2c578014c3a901c4214875
Lucas Ponce <lponce@redhat.com>
2013-02-12 12:53:08 +0100

    Updateing setup-modules in gatein-wcm-impl-sec

--- gatein-wcm-impl-sec/setup-module.sh
+++ gatein-wcm-impl-sec/setup-module.sh
@@ -1,7 +1,7 @@
 # Retrieving JBOSS_HOME
 . $(pwd)/../setup.properties
 
-if [ -f target/gatein-wcm-impl-0.0.1-SNAPSHOT.jar ]; then
-  echo "Copying target/gatein-wcm-impl-0.0.1-SNAPSHOT.jar into $JBOSS_HOME/modules/org/gatein/wcm/gatein-wcm-impl/main"
-  cp target/gatein-wcm-impl-0.0.1-SNAPSHOT.jar $JBOSS_HOME/modules/org/gatein/wcm/gatein-wcm-impl/main
+if [ -f target/gatein-wcm-impl-sec-0.0.1-SNAPSHOT.jar ]; then
+  echo "Copying target/gatein-wcm-impl-sec-0.0.1-SNAPSHOT.jar into $JBOSS_HOME/modules/org/modeshape/main"
+  cp target/gatein-wcm-impl-sec-0.0.1-SNAPSHOT.jar $JBOSS_HOME/modules/org/modeshape/main
 fi
